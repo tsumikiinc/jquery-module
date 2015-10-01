127b1498d233717a3e2d31c20d9c202fbd99b0ff
RyotaHirano <hirano@tsumikiinc.com>
2015-10-01 20:04:21 +0900

    [add] image fit

---
+++ src/es3-image-fit.js
@@ -0,0 +1,82 @@
+'use strict';
+
+import $ from 'jquery';
+var $window = $(window);
+
+module.exports = (function() {
+
+  function Imagefit(el) {
+    this._configure(el);
+  }
+
+  Imagefit.prototype._configure = function(el) {
+    this.$el = $(el);
+    this.option = $.extend({
+      aspectRatio: 9 / 16
+    });
+    this.init();
+  };
+
+  Imagefit.prototype.init = function() {
+    this.$el.css({
+      position: 'absolute',
+      top: '50%',
+      left: '50%'
+    });
+    this.load();
+    this.resize();
+  };
+
+  Imagefit.prototype.load = function() {
+    this._loadWindow();
+  };
+
+  Imagefit.prototype.resize = function() {
+    this._resizeWindow();
+  };
+
+  Imagefit.prototype._fit = function(el) {
+    var containerWidth = $window.width();
+    var containerHeight = $window.height();
+    var currentAspectRatio = containerHeight / containerWidth;
+    var width;
+    var height;
+    var marginTop;
+    var marginLeft;
+
+    if ( this.option.aspectRatio > currentAspectRatio ) {
+        width = containerWidth;
+    } else {
+        width = containerHeight / this.option.aspectRatio;
+    }
+    height = width * this.option.aspectRatio;
+
+    marginTop = -( height / 2 );
+    marginLeft = -( width / 2 );
+
+    el.css({
+      width: width,
+      height: height,
+      marginTop: marginTop,
+      marginLeft: marginLeft
+    });
+    return this;
+  };
+
+  Imagefit.prototype._loadWindow = function() {
+    var _this = this;
+    $(document).ready(function() {
+      _this._fit(_this.$el);
+    });
+  };
+
+  Imagefit.prototype._resizeWindow = function() {
+    var _this = this;
+    $window.on('resize.fit', function() {
+      _this._fit(_this.$el);
+    });
+    return this;
+  };
+
+  return Imagefit;
+})();
